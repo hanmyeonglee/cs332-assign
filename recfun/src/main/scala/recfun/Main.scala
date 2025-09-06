eaefb6adef081aa5217ca392b58029d4e751b0ff
Nemo <ejongwon7@gmail.com>
2025-09-07 00:19:03 +0900

    Feat: rebuilds and tests the project

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -5,17 +5,19 @@ import scala.annotation.tailrec
 
 object Main {
   def main(args: Array[String]) {
-//    println("Pascal's Triangle")
-//    for (row <- 0 to 10) {
-//      for (col <- 0 to row)
-//        print(pascal(col, row) + " ")
-//      println()
-//    }
-//    println(balance("((if (zero? x) max (/ 1 x))".toList))
-//    println(balance("I told him (that it's not (yet) done).\n(But he wasn't listening)".toList))
-//    println(balance(":-)".toList))
-//    println(balance("())(".toList))
-//    println(balance("()()((()))".toList))
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+
+    println(balance("((if (zero? x) max (/ 1 x))".toList))
+    println(balance("I told him (that it's not (yet) done).\n(But he wasn't listening)".toList))
+    println(balance(":-)".toList))
+    println(balance("())(".toList))
+    println(balance("()()((()))".toList))
+
     println(countChange(4, List(1,2)))
   }
 
