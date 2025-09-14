a158e1af91c0d5a583eb7eba20979925b80f2dcf
Nemo <ejongwon7@gmail.com>
2025-09-14 13:44:15 +0900

    Feat: changes wrong implementation and modifies codes simply

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -22,41 +22,31 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-  def singletonSet(elem: Int): Set = {
-    (x: Int) => x == elem
-  }
+  def singletonSet(elem: Int): Set = x => x == elem
   
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: Set, t: Set): Set = {
-    (x: Int) => s(x) || t(x)
-  }
+  def union(s: Set, t: Set): Set = x => s(x) || t(x)
   
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: Set, t: Set): Set = {
-    (x: Int) => s(x) && t(x)
-  }
+  def intersect(s: Set, t: Set): Set = x => s(x) && t(x)
   
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = {
-    (x: Int) => s(x) && !t(x)
-  }
+  def diff(s: Set, t: Set): Set = x => s(x) && !t(x)
   
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: Set, p: Int => Boolean): Set = {
-    (x: Int) => s(x) && p(x)
-  }
+  def filter(s: Set, p: Int => Boolean): Set = x => s(x) && p(x)
   
 
   /**
@@ -82,16 +72,12 @@ object FunSets {
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-  def exists(s: Set, p: Int => Boolean): Boolean = {
-    !forall(s, (x: Int) => !p(x))
-  }
+  def exists(s: Set, p: Int => Boolean): Boolean = !forall(s, (x: Int) => !p(x))
   
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-  def map(s: Set, f: Int => Int): Set = {
-    (x: Int) => s(f(x))
-  }
+  def map(s: Set, f: Int => Int): Set = y => exists(s, x => f(x) == y)
   
   /**
    * Displays the contents of a set
