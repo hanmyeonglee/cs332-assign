2a3ed55dffdfdb15d6cfb54e2916298736bfd746
Nemo <ejongwon7@gmail.com>
2025-10-06 19:20:44 +0900

    Feat: implements sentenceOccurrences

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -40,7 +40,7 @@ object Anagrams {
   }.toList
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = wordOccurrences(s.mkString)
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
