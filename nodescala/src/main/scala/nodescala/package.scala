b56ef50536f69d85b35bb5b6b096f095153f1dfe
Nemo <ejongwon7@gmail.com>
2025-11-07 00:43:04 +0900

    Fix: changes continueWith to more safe way

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -95,11 +95,11 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continueWith[S](cont: Future[T] => S): Future[S] =
-      f.transform(
-        _ => cont(f),
-        e => e
-      )
+    def continueWith[S](cont: Future[T] => S): Future[S] = {
+      val p = Promise[S]()
+      f.onComplete(_ => p.complete(Try(cont(f))))
+      p.future
+    }
 
     /** Continues the computation of this future by taking the result
      *  of the current future and mapping it into another future.
