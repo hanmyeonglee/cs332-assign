9e7e666109d2a7de966308dc35c92313e3135b2b
Nemo <ejongwon7@gmail.com>
2025-11-06 22:01:18 +0900

    Feat: implements now

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -82,7 +82,11 @@ package object nodescala {
      *  However, it is also non-deterministic -- it may throw or return a value
      *  depending on the current state of the `Future`.
      */
-    def now: T = ???
+    def now: T = f.value match {
+      case Some(Success(v)) => v
+      case Some(Failure(e)) => throw e
+      case None => throw new NoSuchElementException("Future not completed")
+    }
 
     /** Continues the computation of this future by taking the current future
      *  and mapping it into another future.
