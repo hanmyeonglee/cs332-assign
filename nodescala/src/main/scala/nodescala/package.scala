225848fb2d9c33e8fa9d1deb4ad488f26248033f
Nemo <ejongwon7@gmail.com>
2025-11-06 22:03:19 +0900

    Feat: implements continueWith

--- nodescala/src/main/scala/nodescala/package.scala
+++ nodescala/src/main/scala/nodescala/package.scala
@@ -94,7 +94,18 @@ package object nodescala {
      *  The function `cont` is called only after the current future completes.
      *  The resulting future contains a value returned by `cont`.
      */
-    def continueWith[S](cont: Future[T] => S): Future[S] = ???
+    def continueWith[S](cont: Future[T] => S): Future[S] = {
+      val p = Promise[S]()
+      f.onComplete { _ =>
+        try {
+          val result = cont(f)
+          p.success(result)
+        } catch {
+          case NonFatal(e) => p.failure(e)
+        }
+      }
+      p.future
+    }
 
     /** Continues the computation of this future by taking the result
      *  of the current future and mapping it into another future.
